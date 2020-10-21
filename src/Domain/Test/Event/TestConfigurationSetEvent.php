9704b35d24569e37fc4853d0022d8f9034004e37
Adrian Lüthi <adi.l@bluewin.ch>
2020-10-21 17:22:39 +0200

    add configurations to test

---
+++ src/Domain/Test/Event/TestConfigurationSetEvent.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace srag\asq\Test\Domain\Test\Event;
+
+use ILIAS\Data\UUID\Uuid;
+use ilDateTime;
+use srag\CQRS\Aggregate\AbstractValueObject;
+use srag\CQRS\Event\AbstractDomainEvent;
+
+/**
+ * Class TestConfigurationSetEvent
+ *
+ * @package srag\asq\Test
+ *
+ * @author studer + raimann ag - Team Core 2 <al@studer-raimann.ch>
+ */
+class TestConfigurationSetEvent extends AbstractDomainEvent
+{
+    const VAR_CONFIG = 'config';
+    const VAR_CONFIG_FOR = 'config_for';
+
+    /**
+     * @var AbstractValueObject
+     */
+    protected $config;
+
+    /**
+     * @var string
+     */
+    protected $config_for;
+
+    /**
+     * @param Uuid $aggregate_id
+     * @param ilDateTime $occured_on
+     * @param int $initiating_user_id
+     * @param AbstractValueObject $data
+     */
+    public function __construct(
+        Uuid $aggregate_id,
+        ilDateTime $occured_on,
+        int $initiating_user_id,
+        AbstractValueObject $config = null,
+        string $config_for = null
+        ) {
+            $this->config = $config;
+            $this->config_for = $config_for;
+            parent::__construct($aggregate_id, $occured_on, $initiating_user_id);
+    }
+
+    /**
+     * @return AbstractValueObject
+     */
+    public function getConfig() : AbstractValueObject
+    {
+        return $this->config;
+    }
+
+    /**
+     * @return string
+     */
+    public function getConfigFor() : string
+    {
+        return $this->config_for;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \srag\CQRS\Event\AbstractDomainEvent::getEventBody()
+     */
+    public function getEventBody() : string
+    {
+        $body = [];
+        $body[self::VAR_CONFIG] = $this->config;
+        $body[self::VAR_CONFIG_FOR] = $this->config_for;
+        return json_encode($body);
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \srag\CQRS\Event\AbstractDomainEvent::restoreEventBody()
+     */
+    protected function restoreEventBody(string $event_body) : void
+    {
+        $body = json_decode($event_body, true);
+        $this->config = AbstractValueObject::createFromArray($body[self::VAR_CONFIG]);
+        $this->config_for = $body[self::VAR_CONFIG_FOR];
+    }
+
+    /**
+     * @return int
+     */
+    public static function getEventVersion() : int
+    {
+        // initial version 1
+        return 1;
+    }
+}
--- src/Domain/Test/Model/Test.php
+++ src/Domain/Test/Model/Test.php
@@ -5,6 +5,8 @@ namespace srag\asq\Test\Domain\Test\Model;
 use srag\CQRS\Aggregate\AbstractAggregateRoot;
 use srag\asq\Test\Domain\Test\Event\TestDataSetEvent;
 use ilDateTime;
+use srag\CQRS\Aggregate\AbstractValueObject;
+use srag\asq\Test\Domain\Test\Event\TestConfigurationSetEvent;
 
 /**
  * Class AssessmentTest
@@ -19,6 +21,11 @@ class AssessmentTest extends AbstractAggregateRoot
      * @var ?TestData
      */
     protected $data;
+
+    /**
+     * @var AbstractValueObject[]
+     */
+    protected $configurations = [];
 
     /**
      * @param TestData $data
@@ -51,5 +58,45 @@ class AssessmentTest extends AbstractAggregateRoot
     protected function applyTestDataSetEvent(TestDataSetEvent $event) : void
     {
         $this->data = $event->getTestData();
+    }
+
+    /**
+     * @param AbstractValueObject $configuration
+     * @param string $configuration_for
+     * @param int $user_id
+     */
+    public function setConfiguration(
+        AbstractValueObject $configuration,
+        string $configuration_for,
+        int $user_id) : void
+    {
+        if (! array_key_exists($configuration_for, $this->configurations) ||
+            ! AbstractValueObject::isNullableEqual($configuration, $this->configurations[$configuration_for])) {
+            $this->ExecuteEvent(
+                new TestConfigurationSetEvent(
+                    $this->aggregate_id,
+                    new ilDateTime(time(), IL_CAL_UNIX),
+                    $user_id,
+                    $configuration,
+                    $configuration_for)
+                );
+        }
+    }
+
+    /**
+     * @param string $configuration_for
+     * @return AbstractValueObject|NULL
+     */
+    public function getConfiguration(string $configuration_for) : ?AbstractValueObject
+    {
+        return $this->configurations[$configuration_for];
+    }
+
+    /**
+     * @param TestConfigurationSetEvent $event
+     */
+    protected function applyTestConfigurationSetEvent(TestConfigurationSetEvent $event) : void
+    {
+        $this->configurations[$event->getConfigFor()] = $event->getConfig();
     }
 }
