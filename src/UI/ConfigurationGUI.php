d7360b664228aa78c8255194e3e981b742228f47
Adrian Lüthi <adi.l@bluewin.ch>
2021-03-12 13:00:24 +0100

    implement config gui display

--- src/UI/ConfigurationGUI.php
+++ src/UI/ConfigurationGUI.php
@@ -3,8 +3,14 @@ declare(strict_types = 1);
 
 namespace srag\asq\Test\UI;
 
+use srag\asq\Application\Service\UIService;
 use srag\asq\Test\Domain\Test\ITest;
 use srag\asq\Test\Domain\Test\Model\AssessmentTestDto;
+use srag\CQRS\Aggregate\AbstractValueObject;
+use ILIAS\UI\Component\Input\Container\Form\Form;
+use ILIAS\DI\UIServices;
+use srag\asq\UserInterface\Web\Form\Factory\IObjectFactory;
+use ilLanguage;
 
 /**
  * Class AsqTestServices
@@ -24,6 +30,11 @@ class ConfigurationGUI
     const CURRENT_CONFIG = 'CurrentConfig';
 
     /**
+     * @var AbstractValueObject
+     */
+    private $current_data;
+
+    /**
      * @var AssessmentTestDto
      */
     private $test;
@@ -34,12 +45,86 @@ class ConfigurationGUI
     private $test_module;
 
     /**
+     * @var Form
+     */
+    private $form;
+
+    /**
+     * @var IObjectFactory[]
+     */
+    private $factories;
+
+    /**
+     * @var UIServices
+     */
+    private $ui;
+
+    /**
+     * @var UIService
+     */
+    private $asq_ui;
+
+    /**
+     * @var ilLanguage
+     */
+    private $language;
+
+    /**
      * @param AssessmentTestDto $test
      */
     public function __construct(AssessmentTestDto $test)
     {
+        global $DIC, $ASQDIC;
+        $this->ui = $DIC->ui();
+        $this->language = $DIC->language();
+        $this->asq_ui = $ASQDIC->asq()->ui();
+
         $this->test = $test;
         $this->test_module = $test->getTestData()->getTest();
+        $current = $_GET[self::CURRENT_CONFIG] ?? reset($this->test_module->getModules())->getType();
+        $this->factories = $this->getCurrentFactories($current);
+        $this->initiateForm($url);
+    }
+
+    /**
+     * @param string $current
+     * @return array
+     */
+    private function getCurrentFactories(string $current) : array
+    {
+        $modules =
+        array_filter(
+            $this->test_module->getModules(),
+            function($module) use($current)
+            {
+                return $module->getType() === $current;
+        });
+
+        $factories = [];
+        foreach ($modules as $module) {
+            $config_class = $module->getConfigClass();
+
+            if ($config_class === null) {
+                continue;
+            }
+
+            $factories[get_class($module)] = new $config_class($this->language, $this->ui, $this->asq_ui);
+        }
+        return $factories;
+    }
+
+    private function initiateForm() : void
+    {
+        $sections = [];
+
+        foreach ($this->factories as $module => $factory) {
+            $sections[] = $this->ui->factory()->input()->field()->section(
+                $factory->getFormfields($this->test->getConfiguration($module)),
+                $module
+            );
+        }
+
+        $this->form = $this->ui->factory()->input()->container()->form()->standard('', $sections);
     }
 
     /**
@@ -64,11 +149,10 @@ class ConfigurationGUI
     }
 
     /**
-     * @param string $module
      * @return string
      */
-    public function render(string $module) : string
+    public function render() : string
     {
-        return 'CONFIG';
+        return $this->ui->renderer()->render($this->form);
     }
 }
