db91997843796a17fa66f031475adbbfd38bb550
Adrian Lüthi <adi.l@bluewin.ch>
2021-11-04 15:14:07 +0100

    finish instance model

--- src/Domain/Instance/Event/RunStartedEvent.php
+++ src/Domain/Instance/Event/RunStartedEvent.php
@@ -1,1 +1,71 @@
 <?php
+declare(strict_types = 1);
+
+namespace Fluxlabs\Assessment\Test\Domain\Instance\Event;
+
+use ILIAS\Data\UUID\Uuid;
+use ilDateTime;
+use Fluxlabs\CQRS\Event\AbstractDomainEvent;
+use ILIAS\Data\UUID\Factory;
+
+/**
+ * Class RunStartedEvent
+ *
+ * @package Fluxlabs\Assessment\Test
+ *
+ * @author Fluxlabs AG - Adrian Lüthi <adi@fluxlabs.ch>
+ */
+class RunStartedEvent extends AbstractDomainEvent
+{
+    const KEY_RUN_ID = 'run_id';
+    const KEY_USER_ID = 'user_id';
+
+    protected ?Uuid $run_id;
+
+    protected ?int $user_id;
+
+    public function __construct(
+        Uuid $aggregate_id,
+        ilDateTime $occurred_on,
+        ?Uuid $run_id = null,
+        ?int $user_id = null
+    ) {
+        parent::__construct($aggregate_id, $occurred_on);
+        $this->run_id = $run_id;
+        $this->user_id = $user_id;
+    }
+
+    public function getRunId() : ?Uuid
+    {
+        return $this->run_id;
+    }
+
+    public function getUserId() : ?int
+    {
+        return $this->user_id;
+    }
+
+    public function getEventBody() : string
+    {
+        $body = [];
+        $body[self::KEY_RUN_ID] = $this->run_id->toString();
+        $body[self::KEY_USER_ID] = $this->user_id;
+        return json_encode($body);
+    }
+
+    protected function restoreEventBody(string $event_body) : void
+    {
+        $factory = new Factory();
+
+        $body = json_decode($event_body, true);
+
+        $this->run_id = $factory->fromString($body[self::KEY_RUN_ID]);
+        $this->user_id = $body[self::KEY_USER_ID];
+    }
+
+    public static function getEventVersion() : int
+    {
+        // initial version 1
+        return 1;
+    }
+}
--- src/Domain/Instance/Model/AssessmentInstance.php
+++ src/Domain/Instance/Model/AssessmentInstance.php
@@ -3,11 +3,16 @@ declare(strict_types = 1);
 
 namespace Fluxlabs\Assessment\Test\Domain\Instance\Model;
 
+use Fluxlabs\Assessment\Test\Domain\Instance\Event\RunCancelledEvent;
+use Fluxlabs\Assessment\Test\Domain\Instance\Event\RunCorrectedEvent;
+use Fluxlabs\Assessment\Test\Domain\Instance\Event\RunStartedEvent;
+use Fluxlabs\Assessment\Test\Domain\Instance\Event\RunSubmitedEvent;
 use Fluxlabs\CQRS\Event\DomainEvent;
 use ILIAS\Data\UUID\Uuid;
 use ilDateTime;
 use Fluxlabs\CQRS\Aggregate\AbstractAggregateRoot;
 use Fluxlabs\CQRS\Event\Standard\AggregateCreatedEvent;
+use srag\asq\Application\Exception\AsqException;
 
 /**
  * Class AssessmentInstance
@@ -21,7 +26,7 @@ class AssessmentInstance extends AbstractAggregateRoot
     const KEY_CONFIG = 'config';
 
     /**
-     * @var AssessmentInstanceRun[]
+     * @var array<int, array<Uuid, AssessmentInstanceRun>>
      */
     protected array $runs;
 
@@ -29,8 +34,7 @@ class AssessmentInstance extends AbstractAggregateRoot
 
     public static function create(
         Uuid $id,
-        AssessmentInstanceConfiguration $configuration,
-        int $user_id) : AssessmentInstance
+        AssessmentInstanceConfiguration $configuration) : AssessmentInstance
     {
         $instance = new AssessmentInstance();
         $occurred_on = new ilDateTime(time(), IL_CAL_UNIX);
@@ -38,7 +42,6 @@ class AssessmentInstance extends AbstractAggregateRoot
             new AggregateCreatedEvent(
                 $id,
                 $occurred_on,
-                $user_id,
                 [
                     self::KEY_CONFIG => $configuration
                 ]
@@ -52,5 +55,99 @@ class AssessmentInstance extends AbstractAggregateRoot
     {
         parent::applyAggregateCreatedEvent($event);
         $this->configuration = $event->getAdditionalData()[self::KEY_CONFIG];
+    }
+
+    public function canUserStartRun(int $user_id) : bool
+    {
+        if (!$this->userHasAvailableTries())
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function startRun(int $user_id, Uuid $run_id) : void
+    {
+        if (!$this->userHasAvailableTries()) {
+            throw new AsqException("User has no avalable tries");
+        }
+
+        $this->ExecuteEvent(
+            new RunStartedEvent(
+                $this->aggregate_id,
+                new ilDateTime(time(), IL_CAL_UNIX),
+                $run_id,
+                $user_id
+            )
+        );
+    }
+
+    protected function cancelRun(int $user_id, Uuid $run_id) : void
+    {
+        $run = $this->getRun($user_id, $run_id);
+
+        $this->ExecuteEvent(
+            new RunCancelledEvent(
+                $this->aggregate_id,
+                new ilDateTime(time(), IL_CAL_UNIX),
+                $run_id,
+                $user_id
+            )
+        );
+    }
+
+    protected function submitRun(int $user_id, Uuid $run_id) : void
+    {
+        $run = $this->getRun($user_id, $run_id);
+
+        $this->ExecuteEvent(
+            new RunSubmitedEvent(
+                $this->aggregate_id,
+                new ilDateTime(time(), IL_CAL_UNIX),
+                $run_id,
+                $user_id
+            )
+        );
+    }
+
+    protected function correctRun(int $user_id, Uuid $run_id) : void
+    {
+        $run = $this->getRun($user_id, $run_id);
+
+        $this->ExecuteEvent(
+            new RunCorrectedEvent(
+                $this->aggregate_id,
+                new ilDateTime(time(), IL_CAL_UNIX),
+                $run_id,
+                $user_id
+            )
+        );
+    }
+
+    private function userHasAvailableTries(int $user_id) : bool
+    {
+        // first try
+        if (!array_key_exists($user_id, $this->runs)) {
+            return true;
+        }
+
+        return count($this->runs[$user_id]) < $this->configuration->getTries();
+    }
+
+    private function getRun(int $user_id, Uuid $run_id) : AssessmentInstanceRun
+    {
+        if (!array_key_exists($user_id, $this->runs) ||
+            !array_key_exists($user_id, $this->runs)) {
+            throw new AsqException(
+                sprintf(
+                    'The run with id : "%s" does not exist for User: "%s"',
+                    $run_id->toString(),
+                    $user_id
+                )
+            );
+        }
+
+        return $this->runs[$user_id][$run_id];
     }
 }
